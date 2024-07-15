4478bd607206ceef9c5884fbeca6f8dbbc1db83a
BrunoGaxiola <A01253874tec.mx>
2024-07-15 11:51:29 -0700

    feat: created AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/home.dart';
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent)
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "To-Do App",
       home: Home(),
     );
